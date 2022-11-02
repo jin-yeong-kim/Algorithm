714c29cfe8b3ba72e5909d4cb7d3f039119cc290
jin-yeong-kim <khy2240@gmail.com>
2022-11-02 19:04:50 +0900

    commit

---
+++ src/코테2022_11_02/getLastElement.java
@@ -0,0 +1,8 @@
+package 코테2022_11_02;
+
+public class getLastElement {
+    public int getLastElement(int[] arr) {
+        // TODO:
+        return arr.length == 0 ? -1 : arr[arr.length -1];
+    }
+}
