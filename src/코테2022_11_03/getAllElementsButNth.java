b3a75be399838eda81c57815eedfaaed3620f5a1
jin-yeong-kim <khy2240@gmail.com>
2022-11-03 16:05:36 +0900

    commit

---
+++ src/코테2022_11_03/getAllElementsButNth.java
@@ -0,0 +1,21 @@
+package 코테2022_11_03;
+
+public class getAllElementsButNth {
+    public int[] getAllElementsButNth(int[] arr, int n) {
+        if (n >= arr.length) {
+            return arr;
+        }
+
+        int[] result = new int[arr.length - 1];
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i == n) {
+                continue;
+            }
+            result[index] = arr[i];
+            index++;
+        }
+        return result;
+    }
+}
