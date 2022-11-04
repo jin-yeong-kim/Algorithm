e852d669aab87520f0f6328a09f0fd5b069f40ae
jin-yeong-kim <khy2240@gmail.com>
2022-11-04 15:18:43 +0900

    commit

---
+++ src/코테2022_11_04/ConvertScoreToGradeWithPlusAndMinus.java
@@ -0,0 +1,53 @@
+package 코테2022_11_04;
+
+public class ConvertScoreToGradeWithPlusAndMinus {
+    public String convertScoreToGradeWithPlusAndMinus(int score) {
+        // TODO:
+        if(score > 100 || score < 0){
+            return "INVALID SCORE";
+        }
+        else if(score <= 100 && score >= 90){
+            if((score % 10) >= 8 || (score % 10) == 0){
+                if(score == 90){
+                    return "A-";
+                } else
+                    return "A+";
+            } else if((score % 10) < 3){
+                return "A-";
+            } else return "A";
+        }
+        else if(score <= 89 && score >= 80){
+            if((score % 10) >= 8 || (score % 10) == 0){
+                if(score == 80){
+                    return "B-";
+                } else
+                    return "B+";
+            } else if((score % 10) < 3){
+                return "B-";
+            } else return "B";
+        }
+        else if(score <= 79 && score >= 70){
+            if((score % 10) >= 8 || (score % 10) == 0){
+                if(score == 70){
+                    return "C-";
+                } else
+                    return "C+";
+            } else if((score % 10) < 3){
+                return "C-";
+            } else return "C";
+        }
+        else if(score <= 69 && score >= 60){
+            if((score % 10) >= 8 || (score % 10) == 0){
+                if(score == 60){
+                    return "D-";
+                } else
+                    return "D+";
+            } else if((score % 10) < 3){
+                return "D-";
+            } else return "D";
+        }
+        else {
+            return "F";
+        }
+    }
+}
