59aec8eb448de4556d4c52faac49fd681a87793d
jin-yeong-kim <khy2240@gmail.com>
2022-11-04 15:17:38 +0900

    commit

---
+++ src/코테2022_11_04/isPythagorean.java
@@ -0,0 +1,14 @@
+package 코테2022_11_04;
+
+public class isPythagorean {
+    public boolean isPythagorean(int side1, int side2, int side3) {
+        // TODO:
+        if(side2 * side2 + side1 * side1 == side3 * side3){
+            return true;
+        } else if(side2 * side2 + side3 * side3 == side1 * side1){
+            return true;
+        } else if(side3 * side3 + side1 * side1 == side2 * side2){
+            return true;
+        } else return false;
+    }
+}
