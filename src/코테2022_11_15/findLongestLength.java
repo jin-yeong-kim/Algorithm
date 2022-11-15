1b9f848405329cb83b7bf2b913b6a3e6eb00314d
jin-yeong-kim <khy2240@gmail.com>
2022-11-15 16:03:42 +0900

    commit

---
+++ src/코테2022_11_15/findLongestLength.java
@@ -0,0 +1,19 @@
+package 코테2022_11_15;
+
+import java.util.Arrays;
+
+public class findLongestLength {
+    public static void main(String[] args) {
+        String[] strArr = {"codestates", "java", "backend", "programming"};
+        int output = findLongestLength(strArr);
+        System.out.println(output); // 11 (가장 긴 문자열 : programming)
+    }
+    public static int findLongestLength(String[] strArr) {
+        //TODO..
+        // 빈 배열인 경우 0리턴
+        // 스트림 생성
+        // 맵핑
+        // 최대값 리턴
+        return Arrays.stream(strArr).mapToInt(s -> s.length()).max().orElse(0);
+    }
+}
