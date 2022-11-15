d4a635b0ca776ca76b3ad23dd743905746aaea4c
jin-yeong-kim <khy2240@gmail.com>
2022-11-15 11:46:44 +0900

    commit

---
+++ src/코테2022_11_15/ComputeAverageOfNumbers.java
@@ -0,0 +1,18 @@
+package 코테2022_11_15;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ComputeAverageOfNumbers {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        double output = Solution.computeAverageOfNumbers(list);
+        System.out.println(output); // --> 3
+    }
+    public class Solution {
+        public static double computeAverageOfNumbers(List<Integer> list) {
+            //TODO..
+            return list.stream().mapToDouble(d -> d).average().orElse(0);
+        }
+    }
+}
