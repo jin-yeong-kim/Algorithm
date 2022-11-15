a99c75a1a58b1bca6eb69e65b77713a98d8aed5d
jin-yeong-kim <khy2240@gmail.com>
2022-11-15 16:06:27 +0900

    commit

---
+++ src/코테2022_11_15/IsHot.java
@@ -0,0 +1,16 @@
+package 코테2022_11_15;
+
+import java.util.Arrays;
+
+public class IsHot {
+    public static void main(String[] args) {
+        int[] temperature = {25, 29, 30, 31, 26, 30, 33};
+        boolean output = isHot(temperature);
+        System.out.println(output); // true
+    }
+    public static boolean isHot(int[] temperature) {
+        //TODO..
+        if(temperature.length > 7) return false;
+        return Arrays.stream(temperature).filter(n -> n >= 30).count() >= 3 ? true : false;
+    }
+}
