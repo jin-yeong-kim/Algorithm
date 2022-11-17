92cc8739b6c09094d86803e936d855b8d704c5dd
jin-yeong-kim <khy2240@gmail.com>
2022-11-17 15:42:26 +0900

    commit

---
+++ src/코테2022_11_17/SumTo.java
@@ -0,0 +1,12 @@
+package 코테2022_11_17;
+
+public class SumTo {
+    public static void main(String[] args) {
+        System.out.println(sumTo(10));
+    }
+    public static int sumTo(int num){
+        //TODO..
+        if(num == 0) return 0;
+        return num + sumTo(num - 1);
+    }
+}
