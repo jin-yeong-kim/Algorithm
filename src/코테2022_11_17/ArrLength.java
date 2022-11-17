b510ec176251dd4dccdf2d64280572b6b0cff0c2
jin-yeong-kim <khy2240@gmail.com>
2022-11-17 15:47:57 +0900

    commit

---
+++ src/코테2022_11_17/ArrLength.java
@@ -0,0 +1,17 @@
+package 코테2022_11_17;
+
+public class ArrLength {
+    public static void main(String[] args) {
+        int output = arrLength(new int[]{1, -2, 1, 3});
+        System.out.println(output);
+    }
+    static int count = 0;
+    public static int arrLength(int[] arr){
+        // TODO:
+        if(arr.length == 0) return count;
+        count++;
+        int[] a = new int[arr.length - 1];
+        System.arraycopy(arr,1,a,0,arr.length-1);
+        return arrLength(a);
+    }
+}
