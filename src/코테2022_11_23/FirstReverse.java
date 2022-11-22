ec6a2c7f1dd1d8b37e537117b87f02d5b1cc2d47
jin-yeong-kim <khy2240@gmail.com>
2022-11-23 08:13:07 +0900

    commit

---
+++ src/코테2022_11_23/FirstReverse.java
@@ -0,0 +1,18 @@
+package 코테2022_11_23;
+
+/*문제
+문자열을 입력받아 순서가 뒤집힌 문자열을 리턴해야 합니다.*/
+
+public class FirstReverse {
+    public static void main(String[] args) {
+        String output = firstReverse("codestates");
+        System.out.println(output);
+    }
+    public static String firstReverse(String str) {
+        // TODO:
+        StringBuilder sb = new StringBuilder();
+        sb.append(str).reverse();
+        String result = sb.toString();
+        return result;
+    }
+}
