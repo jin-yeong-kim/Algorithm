eaf168ec7cdc0ddc4eb184a909c737a9bc6a9e47
jin-yeong-kim <khy2240@gmail.com>
2023-01-22 20:26:16 +0900

    coding

---
+++ src/코테23년01월/코테2023_01_22/PrimeFactorization.java
@@ -0,0 +1,36 @@
+package 코테23년01월.코테2023_01_22;
+
+/*
+문제
+정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수 N (1 ≤ N ≤ 10,000,000)이 주어진다.
+
+출력
+N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. N이 1인 경우 아무것도 출력하지 않는다.
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PrimeFactorization {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            while (N % i == 0) {
+                sb.append(i).append('\n');
+                N /= i;
+            }
+        }
+        if (N != 1) {
+            sb.append(N);
+        }
+        System.out.println(sb);
+    }
+}
