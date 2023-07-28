be908d574f4a8de303eee631fa87ed689b9edf5d
jin-yeong-kim <khy2240@gmail.com>
2023-07-28 21:54:04 +0900

    coding

---
+++ src/코테23년07월/코테2023_07_28/Rectangle.java
@@ -0,0 +1,12 @@
+package 코테23년07월.코테2023_07_28;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Rectangle {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Long.parseLong(br.readLine()) * Long.parseLong(br.readLine()));
+    }
+}
