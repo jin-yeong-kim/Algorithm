6979f60dc33f512e506ab154298b6a461235b086
jin-yeong-kim <khy2240@gmail.com>
2023-09-14 16:34:16 +0900

    coding

---
+++ src/코테23년09월/코테2023_09_14/Beakjoon11005.java
@@ -0,0 +1,29 @@
+package 코테23년09월.코테2023_09_14;
+
+import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Beakjoon11005 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        List<Character> result = new ArrayList<>();
+        while (N > 0) {
+            if (N % B < 10) {
+                result.add((char) (N % B + '0'));
+            } else {
+                result.add((char) (N % B - 10 + 'A'));
+            }
+            N /= B;
+        }
+        for (int i = result.size() - 1; i >= 0; i--) {
+            System.out.print(result.get(i));
+        }
+    }
+}
