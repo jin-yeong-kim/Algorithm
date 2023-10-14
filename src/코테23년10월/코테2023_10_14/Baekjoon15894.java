745f3c44b301191975e7f26641ebf49823e8f32d
jin-yeong-kim <khy2240@gmail.com>
2023-10-14 10:16:09 +0900

    coding

---
+++ src/코테23년10월/코테2023_10_14/Baekjoon15894.java
@@ -0,0 +1,12 @@
+package 코테23년10월.코테2023_10_14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon15894 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(4 * Long.parseLong(br.readLine()));
+    }
+}
