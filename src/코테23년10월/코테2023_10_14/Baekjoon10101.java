5d8163bb189a401e570ac0fc2cfbf2fddb97aaa1
jin-yeong-kim <khy2240@gmail.com>
2023-10-16 15:32:59 +0900

    coding

--- src/코테23년10월/코테2023_10_14/Baekjoon10101.java
+++ src/코테23년10월/코테2023_10_14/Baekjoon10101.java
@@ -7,35 +7,16 @@ import java.io.InputStreamReader;
 public class Baekjoon10101 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int EQCount = 0;
-        int Equal = 0;
-        int Total = 0;
-        int[] Input = new int[3];
-        for (int i = 0; i < 3; i++) {
-            int num = Integer.parseInt(br.readLine());
-            Input[i] = num;
-            Total += num;
-            if (num == 60) {
-                EQCount++;
-            }
-        }
-        if (Input[0] == Input[1]) {
-            Equal++;
-        }
-        if (Input[1] == Input[2]) {
-            Equal++;
-        }
-        if (Input[0] == Input[2]) {
-            Equal++;
-        }
-        if (EQCount == 3) {
-            System.out.println("Equilateral");
-        } else if (Total == 180 && Equal == 1) {
-            System.out.println("Isosceles");
-        } else if (Total == 180 && Equal == 0) {
-            System.out.println("Scalene");
-        } else if (Total != 180) {
+        int A = Integer.parseInt(br.readLine()), B = Integer.parseInt(br.readLine()),
+                C = Integer.parseInt(br.readLine());
+        if (A + B + C != 180) {
             System.out.println("Error");
+        } else if (A == 60 && B == 60 && C == 60) {
+            System.out.println("Equilateral");
+        } else if (A == B || B == C || A == C) {
+            System.out.println("Isosceles");
+        } else {
+            System.out.println("Scalene");
         }
     }
 }
