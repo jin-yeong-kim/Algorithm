3adfde01ad116a72a6fe47c8a63d2bc59b506985
jin-yeong-kim <khy2240@gmail.com>
2023-10-19 14:52:07 +0900

    coding

---
+++ src/코테23년10월/코테2023_10_19/Baekjoon24262.java
@@ -0,0 +1,10 @@
+package 코테23년10월.코테2023_10_19;
+
+import java.io.IOException;
+
+public class Baekjoon24262 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("1");
+        System.out.println("0");
+    }
+}
